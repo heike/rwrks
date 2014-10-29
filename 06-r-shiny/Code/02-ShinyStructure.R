8e66680ba6cb1794c5383344d9bf2cec0cc4f265
Anonymous@example.com <cpsievert1@gmail.com>
2014-10-29 17:40:52 -0500

    Remove shiny structure

--- 06-r-shiny/Code/02-ShinyStructure.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-
-### Applet 2 - Verbatim Text Output and Table Output
-runExample("02_text")
-
-### Applet 3 - Reactive Expressions (Title, Verbatim Text, and Table Output)
-runExample("03_reactivity")
