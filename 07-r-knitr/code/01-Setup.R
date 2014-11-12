c163ceb533361f6833c6e3cbd82b6214b1c6ecc9
Anonymous@example.com <cpsievert1@gmail.com>
2014-11-12 17:39:10 -0600

    new links

--- 07-r-knitr/code/01-Setup.R
+++ 07-r-knitr/code/01-Setup.R
@@ -3,6 +3,11 @@
 install.packages("knitr", dependencies=TRUE)
 install.packages(c("Hmisc", "xtable"))
 
+install.packages("rmarkdown")
+# if you can't install rmarkdown, you'll at least want markdown
+install.packages("markdown")
+
 library(knitr)
 library(Hmisc)
 library(xtable)
+
