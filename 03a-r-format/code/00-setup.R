7deec213ce505d41ce2d6f88aea8b376c3edfd21
Eric Hare <ericrhare@gmail.com>
2015-06-10 08:54:27 -0500

    We need lubridate as well

--- 03a-r-format/code/00-setup.R
+++ 03a-r-format/code/00-setup.R
@@ -5,6 +5,7 @@ install.packages("dplyr")
 install.packages("tidyr")
 install.packages("gdata")
 install.packages("foreign")
+install.packages("lubridate")
 
 # Or if you are working on the terminal servers...
 library(reshape2)
@@ -13,3 +14,4 @@ library(dplyr)
 library(tidyr)
 library(gdata)
 library(foreign)
+library(lubridate)
