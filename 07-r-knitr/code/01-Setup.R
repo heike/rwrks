9cbfc31307d1124e6a6ac0fedd43da70feb1d3be
Carson Sievert <cpsievert1@gmail.com>
2015-06-11 09:53:23 -0500

    update rmd setuo

--- 07-r-knitr/code/01-Setup.R
+++ 07-r-knitr/code/01-Setup.R
@@ -4,3 +4,18 @@ install.packages("pander", dependencies = TRUE)
 library(rmarkdown)
 library(pander)
 
+# you'll want to use RStudio...
+stopifnot(Sys.getenv("RSTUDIO") == "1")
+
+# ----------------------------------------------------------------------------
+# Optional
+# ----------------------------------------------------------------------------
+
+install_pkg <- function(x) {
+  if (!requireNamespace(x)) install.packages(x)
+}
+install_pkg("animation")
+install_pkg("DT")
+install_pkg("devtools")
+devtools::install_github("rstudio/rticles")
+devtools::install_github("ropensci/plotly@carson-dsl")
