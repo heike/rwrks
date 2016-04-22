42b03a1001fadfb50977bb89817d078b723472af
Eric Hare <ericrhare@gmail.com>
2016-04-22 08:21:41 -0500

    Some more updates to the plot maps

--- 02-r-graphics/code/0-Setup.R
+++ 02-r-graphics/code/0-Setup.R
@@ -1,13 +1,13 @@
 packages <- c("ggplot2", "gridExtra", "lubridate",
-              "maps","ggmap", "mapproj")
+              "maps","ggmap", "mapproj", "dplyr")
 
-#install.packages(packages, dep=TRUE)
+install.packages(packages, dep=TRUE)
 
 library(ggplot2)
 library(lubridate)
 library(maps)
 library(ggmap)
-
+library(dplyr)
 
 today()
 qplot(rnorm(100), rnorm(100), colour=factor(rbinom(100, 1, 0.5)))
