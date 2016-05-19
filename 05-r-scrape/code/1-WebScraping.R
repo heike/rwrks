d8a5fcfe142ee8bbeb37fc8846de0ffc3eb4d066
Andee Kaplan <ajkaplan@iastate.edu>
2016-05-18 20:03:53 -0500

    add correct r script

--- 05-r-scrape/code/1-WebScraping.R
+++ 05-r-scrape/code/1-WebScraping.R
@@ -1,50 +1,71 @@
+## ---- warning=FALSE, message=FALSE---------------------------------------
 library(rvest)
 src <- read_html("http://en.wikipedia.org/wiki/Table_(information)")
 node <- html_node(src, css = ".wikitable")
 
+## ------------------------------------------------------------------------
 html_table(node)
 
+## ------------------------------------------------------------------------
 read_html("http://en.wikipedia.org/wiki/Table_(information)") %>%
   html_node(".wikitable") %>% html_table()
 
+## ------------------------------------------------------------------------
 domain <- "http://www.sec.gov"
 susp <- paste0(domain, "/litigation/suspensions.shtml")
 hrefs <- read_html(susp) %>% html_nodes("p+ table a") %>% html_attr(name = "href")
 tail(hrefs)
 
-# returns the HTML page source, which is _static_ (no table!)
-rvest::html("http://bl.ocks.org/cpsievert/raw/2a9fb8f504cd56e9e8e3/")
+## ---- eval=FALSE---------------------------------------------------------
+## hrefs <- hrefs[!is.na(hrefs)]
+## pdfs <- paste0(domain, hrefs)[1:10]
+## 
+## for (i in 1:length(pdfs)) {
+##     download.file(url = pdfs[i], destfile = basename(pdfs[i]))
+## }
 
+## ------------------------------------------------------------------------
 library(httr)
-hadley <- GET("https://api.github.com/users/hadley")
-content(hadley)[c("name", "company")]
+andee <- GET("https://api.github.com/users/andeek")
+content(andee)[c("name", "company")]
 
-hadley <- httr::GET("https://api.github.com/users/hadley")
-hadley$header[1:3]
+## ------------------------------------------------------------------------
+andee$header[1:3]
 
+## ---- echo=FALSE---------------------------------------------------------
+# hopefully no one is watching
 library(XML2R)
-obs <- XML2Obs("http://bit.ly/mario-xml", quiet = TRUE)
+obs <- XML2Obs("https://gist.githubusercontent.com/cpsievert/85e340814cb855a60dc4/raw/651b7626e34751c7485cff2d7ea3ea66413609b8/mariokart.xml", quiet = TRUE)
 table(names(obs))
 
+## ------------------------------------------------------------------------
 obs # named list of observations
 
+## ------------------------------------------------------------------------
 collapse_obs(obs) # group into table(s) by observational name/unit
 
+## ------------------------------------------------------------------------
+library(dplyr)
 obs <- add_key(obs, parent = "mariokart//driver", recycle = "name")
 collapse_obs(obs)
 
+## ------------------------------------------------------------------------
 tabs <- collapse_obs(obs)
-merge(tabs[[1]], tabs[[2]], by = "name")
+left_join(as.data.frame(tabs[[1]]), as.data.frame(tabs[[2]])) %>% select(-url)
 
+## ------------------------------------------------------------------------
 library(jsonlite)
 mario <- fromJSON("http://bit.ly/mario-json")
 str(mario) # nested data.frames?!? 
 
+## ------------------------------------------------------------------------
 mario$driver
 mario$vehicles
 
-# this mapply statement is essentially equivalent to add_key
-vehicles <- mapply(function(x, y) cbind(x, driver = y), 
-                   mario$vehicles, mario$driver, SIMPLIFY = FALSE)
-Reduce(rbind, vehicles)
-mario[!grepl("vehicle", names(mario))]
+## ------------------------------------------------------------------------
+vehicles <- rbind(mario$vehicles[[1]], mario$vehicles[[2]])
+vehicles <- cbind(driver = mario$driver, vehicles)
+
+## ------------------------------------------------------------------------
+workshop_commits_raw <- fromJSON("https://api.github.com/repos/heike/rwrks/commits")
+
