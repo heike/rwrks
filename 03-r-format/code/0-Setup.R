e7571c3feafea60a11b45934054d7d226d58c213
Joe Papio <Joe Papio>
2017-06-02 14:58:32 -0500

    switched install dot packages to tidyverse

--- 03-r-format/code/0-Setup.R
+++ 03-r-format/code/0-Setup.R
@@ -1,15 +1,18 @@
-install.packages("ggplot2")
+install.packages("tidyverse")
+
+#install.packages("ggplot2")
 # installs a few other packages as well
-install.packages("dplyr")
-install.packages("tidyr")
+#install.packages("dplyr")
+#install.packages("tidyr")
 install.packages("readxl")
 install.packages("foreign")
 install.packages("lubridate")
 
 # Or if you are working on the terminal servers...
-library(ggplot2)
-library(dplyr)
-library(tidyr)
+library(tidyverse)
+#library(ggplot2)
+#library(dplyr)
+#library(tidyr)
 library(readxl)
 library(foreign)
 library(lubridate)
